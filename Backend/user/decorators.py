4a6d1be14f54613476a248fd40dd9e70b95493d2
Somaiya <4111674@upm.edu.sa>
2024-02-06 02:46:03 +0300

    auto-format

--- Backend/user/decorators.py
+++ Backend/user/decorators.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.http import JsonResponse
 
+
 # Registerd/Unregisterd users privilges for both sign up or login
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
@@ -9,19 +10,22 @@ def unauthenticated_user(view_func):
             print('')
         else:
             return view_func(request, *args, **kwargs)
+
     return wrapper_func
+
 
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
             group = None
             if request.users.groups.exists():
-               group = request.user.groups.all()[0].name
+                group = request.user.groups.all()[0].name
 
-            
             if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
             else:
                 return JsonResponse({'error': 'Not Authorized'})
-        return wrapper_func 
+
+        return wrapper_func
+
     return decorator
