3454408e8f9ff137374d581a561b72615748796c
Somaiya <4111674@upm.edu.sa>
2024-02-06 02:37:24 +0300

    auth forms

--- Backend/order/serializers.py
+++ Backend/order/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from .models import Order
 
-#send all data as jason
+
+# send all data as jason
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Order
-        fields = '__all__' #can choose some fields not all
+        fields = "__all__"  # can choose some fields not all
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <p>
+        <h1 className='text-center'>Home Page</h1>
+      </p>
+    </div>
+  );
+}
+export default Home;
