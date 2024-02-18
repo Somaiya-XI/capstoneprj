a76cd014fbe85695cf846b0545b7fbe0f8a05a71
my name <heybassant@gmail.com>
2024-02-19 00:52:55 +0300

    AcceptChanges

---
+++ Frontend/src/pages/Account/UserContextProvider.jsx
@@ -0,0 +1,26 @@
+import UserContext from '../../hooks/UserContext';
+import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
+
+const UserContextProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [csrf, setCSRF] = useState('');
+  const [role, setRole] = useState('');
+
+  const userValues = {
+    csrf,
+    setCSRF,
+    isAuthenticated,
+    setIsAuthenticated,
+    role,
+    setRole,
+  };
+
+  return (
+    <UserContext.Provider value={userValues}>
+      <Outlet />
+      {children}
+    </UserContext.Provider>
+  );
+};
+export default UserContextProvider;
