08132fda08c3d6de63943bebb87da39913b3b26e
my name <heybassant@gmail.com>
2024-02-18 12:09:21 +0300

    Supplier Dashboard Initial

--- Frontend/src/pages/Supplier/Components/SideNav.jsx
+++ Frontend/src/pages/Supplier/Components/SideNav.jsx
@@ -1,20 +1,38 @@
-import {Layout} from 'antd';
-const {Header, Sider} = Layout;
-
+import { useState } from 'react';
+import { Button, Layout } from 'antd';
+const { Header, Sider } = Layout;
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined
+}
+    from '@ant-design/icons'
 
 import Logo from './Logo';
 import MenuList from './MenuList';
 function SideNav() {
+    const [collapsed, setCollapsed] = useState(false)
+    
     return (
-        <>
-        <Layout>
-            <Sider className="sidebar"><Logo></Logo>
-            <MenuList/>
-            </Sider>
-        </Layout>
-        </>
         
+            <Layout>
+                <Sider collapsed= {collapsed} 
+                collapsible 
+                trigger={null}
+                className="sidebar"><Logo/>
+                    <MenuList />
+                </Sider>
+                <Layout>
+                    <Header className="Fold">
+                        <Button type='text' 
+                         className='toggle'
+                         onClick={()=> setCollapsed(!collapsed)}
+                         icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined/> } />
+                    </Header>
+                </Layout>
+            </Layout>
+        
+
     );
-  }
-  
-  export default SideNav;
+}
+
+export default SideNav;
