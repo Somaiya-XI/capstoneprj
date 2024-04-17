deb23a0eb3a43ce4bebed82984ec47ebe1a68ce6
my name <heybassant@gmail.com>
2024-04-18 02:25:41 +0300

    -

--- Frontend/src/url.jsx
+++ Frontend/src/url.jsx
@@ -2,6 +2,7 @@ import Products from './pages/Supplier/Components/Products/Products';
 import AddProduct from './pages/Supplier/Components/Products/AddProduct';
 import EditProduct from './pages/Supplier/Components/Products/EditProduct';
 import Orders from './pages/Supplier/Components/Orders';
+import RetailerLayout from './pages/Retailer/RetailerLayout';
 
 import Home from './pages/Home/Home.jsx';
 import ProductDetail from './pages/ProductDetail/ProductDetail.jsx';
@@ -21,6 +22,7 @@ import Schedule from './pages/Supplier/Components/Schedule/Schedule';
 
 
 export {
+  RetailerLayout,
   AddProduct,
   Schedule,
   Products,
